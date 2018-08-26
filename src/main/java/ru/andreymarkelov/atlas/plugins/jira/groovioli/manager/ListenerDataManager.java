27ad0a64e3c3eeafadf04c8982e03ef72f754096
amarkelov <andrey.markelov@infobip.com>
2018-08-26 17:37:13 +0300

    Merge branch 'master' of https://github.com/AndreyVMarkelov/jira-groovioli into Listener
    # Conflicts:
    #	src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/action/admin/ListenersAddAction.java

--- src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/manager/ListenerDataManager.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/manager/ListenerDataManager.java
@@ -3,6 +3,7 @@ package ru.andreymarkelov.atlas.plugins.jira.groovioli.manager;
 import java.util.List;
 
 import com.atlassian.activeobjects.tx.Transactional;
+import ru.andreymarkelov.atlas.plugins.jira.groovioli.data.EventType;
 import ru.andreymarkelov.atlas.plugins.jira.groovioli.data.ListenerData;
 
 @Transactional
@@ -11,4 +12,5 @@ public interface ListenerDataManager {
     void deleteListener(Integer listenerId);
     List<ListenerData> getAll();
     ListenerData get(Integer listenerId);
+    List<String> getScript(EventType eventType, Long projectId);
 }
--- src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/manager/impl/ListenerDataManagerImpl.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/manager/impl/ListenerDataManagerImpl.java
@@ -1,19 +1,28 @@
 package ru.andreymarkelov.atlas.plugins.jira.groovioli.manager.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.atlassian.activeobjects.external.ActiveObjects;
 import net.java.ao.Query;
+import ru.andreymarkelov.atlas.plugins.jira.groovioli.data.EventType;
 import ru.andreymarkelov.atlas.plugins.jira.groovioli.data.ListenerData;
 import ru.andreymarkelov.atlas.plugins.jira.groovioli.data.ao.ListenerDataAO;
 import ru.andreymarkelov.atlas.plugins.jira.groovioli.manager.ListenerDataManager;
 
+import static java.util.Collections.emptyList;
+
 public class ListenerDataManagerImpl implements ListenerDataManager {
+    private static final Long NO_PROJECT = -1L;
+
     private final ActiveObjects ao;
+    private final Map<String, Map<Long, List<String>>> eventMap;
 
     public ListenerDataManagerImpl(ActiveObjects ao) {
         this.ao = ao;
+        this.eventMap = new HashMap<>();
     }
 
     @Override
@@ -25,6 +34,7 @@ public class ListenerDataManagerImpl implements ListenerDataManager {
         listenerDataAO.setScript(listenerData.getScript());
         listenerDataAO.setCreated(listenerData.getCreated());
         listenerDataAO.save();
+        rebuildCache();
     }
 
     @Override
@@ -33,6 +43,7 @@ public class ListenerDataManagerImpl implements ListenerDataManager {
             ListenerDataAO listenerDataAO = ao.get(ListenerDataAO.class, listenerId);
             if (listenerDataAO != null) {
                 ao.delete(listenerDataAO);
+                rebuildCache();
             }
         }
     }
@@ -70,5 +81,37 @@ public class ListenerDataManagerImpl implements ListenerDataManager {
             );
         }
         return null;
+    }
+
+    @Override
+    public List<String> getScript(EventType eventType, Long projectId) {
+        Map<Long, List<String>> projectMap = eventMap.get(eventType.name());
+        if (projectMap != null) {
+            List<String> scripts = projectMap.get(projectId);
+            if (scripts != null) {
+                return scripts;
+            }
+        }
+        return emptyList();
+    }
+
+    private void rebuildCache() {
+        eventMap.clear();
+        List<ListenerData> listenerDataList = getAll();
+        for (ListenerData listenerData : listenerDataList) {
+            final String event = listenerData.getEvent();
+            final Long projectId = (listenerData.getProjectId() != null) ? listenerData.getProjectId() : NO_PROJECT;
+            if (!eventMap.containsKey(event)) {
+                eventMap.put(event, new HashMap<Long, List<String>>());
+            }
+
+            Map<Long, List<String>> projectMap = eventMap.get(event);
+            if (!projectMap.containsKey(projectId)) {
+                projectMap.put(projectId, new ArrayList<String>());
+            }
+
+            List<String> projectScripts = projectMap.get(projectId);
+            projectScripts.add(listenerData.getScript());
+        }
     }
 }
