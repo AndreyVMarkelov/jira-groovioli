6201d674dab5c836b6b91d45f5a1f405b3c8d341
andreymarkelov <Chinus_(123)>
2018-10-13 18:22:46 +0300

    Add http handler admin

--- src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/servlet/HttpHandlerServlet.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/servlet/HttpHandlerServlet.java
@@ -1,16 +1,36 @@
 package ru.andreymarkelov.atlas.plugins.jira.groovioli.servlet;
 
+import org.apache.commons.lang3.ArrayUtils;
+import ru.andreymarkelov.atlas.plugins.jira.groovioli.manager.RestDataManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.apache.commons.lang3.StringUtils.split;
+
 public class HttpHandlerServlet extends HttpServlet {
+    private final RestDataManager restDataManager;
+
+    public HttpHandlerServlet(RestDataManager restDataManager) {
+        this.restDataManager = restDataManager;
+    }
+
     @Override
     protected void service(
             HttpServletRequest req,
             HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getPathInfo();
+        String[] pathParts = split(path, '/');
+
+        if (ArrayUtils.isEmpty(pathParts)) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "At least one path after /groovioli should be set");
+        }
+
+
+
         super.service(req, resp);
     }
 }
