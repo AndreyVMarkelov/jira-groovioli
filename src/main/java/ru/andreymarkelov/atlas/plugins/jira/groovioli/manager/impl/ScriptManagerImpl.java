1e03f6d7d091fe43f8df348803c0d7ab7b238f82
andreymarkelov <Chinus_(123)>
2018-10-23 00:35:04 +0300

    init field

--- src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/manager/impl/ScriptManagerImpl.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/jira/groovioli/manager/impl/ScriptManagerImpl.java
@@ -2,14 +2,21 @@ package ru.andreymarkelov.atlas.plugins.jira.groovioli.manager.impl;
 
 import com.atlassian.jira.config.ConstantsManager;
 import com.atlassian.jira.config.SubTaskManager;
+import com.atlassian.jira.config.properties.ApplicationProperties;
 import com.atlassian.jira.issue.AttachmentManager;
 import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.IssueFactory;
 import com.atlassian.jira.issue.IssueManager;
+import com.atlassian.jira.issue.changehistory.ChangeHistoryManager;
 import com.atlassian.jira.issue.comments.CommentManager;
+import com.atlassian.jira.issue.customfields.manager.OptionsManager;
+import com.atlassian.jira.issue.label.LabelManager;
 import com.atlassian.jira.issue.link.IssueLinkManager;
+import com.atlassian.jira.issue.vote.VoteManager;
 import com.atlassian.jira.issue.watchers.WatcherManager;
+import com.atlassian.jira.issue.worklog.WorklogManager;
 import com.atlassian.jira.project.ProjectManager;
+import com.atlassian.jira.project.version.VersionManager;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.security.groups.GroupManager;
 import com.atlassian.jira.security.roles.ProjectRoleManager;
@@ -60,7 +67,14 @@ public class ScriptManagerImpl implements ScriptManager {
             IssueManager issueManager,
             SubTaskManager subTaskManager,
             ConstantsManager constantsManager,
-            ProjectManager projectManager) {
+            ProjectManager projectManager,
+            LabelManager labelManager,
+            ChangeHistoryManager changeHistoryManager,
+            VoteManager voteManager,
+            WorklogManager worklogManager,
+            VersionManager versionManager,
+            OptionsManager optionsManager,
+            ApplicationProperties applicationProperties) {
         baseVariables = new HashMap<>();
         baseVariables.put("groupManager", groupManager);
         baseVariables.put("watcherManager", watcherManager);
@@ -77,6 +91,13 @@ public class ScriptManagerImpl implements ScriptManager {
         baseVariables.put("subTaskManager", subTaskManager);
         baseVariables.put("constantsManager", constantsManager);
         baseVariables.put("projectManager", projectManager);
+        baseVariables.put("labelManager", labelManager);
+        baseVariables.put("changeHistoryManager", changeHistoryManager);
+        baseVariables.put("voteManager", voteManager);
+        baseVariables.put("worklogManager", worklogManager);
+        baseVariables.put("versionManager", versionManager);
+        baseVariables.put("optionsManager", optionsManager);
+        baseVariables.put("applicationProperties", applicationProperties);
     }
 
     @Override
